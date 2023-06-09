4adc6a6be2a7ec10778183840768f56cb3932987
Igor Kubota <igor-eiki@hotmail.com>
2023-06-08 23:27:05 -0300

    Finished API + DB

--- project.clj
+++ project.clj
@@ -3,8 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :main ^:skip-aot clojure-mysql-api.core
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/java.jdbc "0.7.9"]
+                 [ring/ring-core "1.10.0"]
+                 [lynxeyes/dotenv "1.1.0"]
+                 [ring/ring-jetty-adapter "1.10.0"]
+                 [compojure "1.6.2"]
+                 [mysql/mysql-connector-java "8.0.21"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler clojure-mysql-api.core/app
+         :init clojure-mysql-api.core/init}
+  :main clojure-mysql-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- src/clojure_mysql_api/core.clj
+++ src/clojure_mysql_api/core.clj
@@ -1,7 +1,38 @@
 (ns clojure-mysql-api.core
-  (:gen-class))
+  (:gen-class)
+  (:require [ring.adapter.jetty :as jetty]
+          [ring.util.response :refer [response]]
+          [dotenv :refer [env app-env]]
+          [compojure.route :as route]
+          [compojure.core :refer [defroutes GET]]
+          [clojure.java.jdbc :as jdbc]))
+
+
+
+
+(def db {
+  :classname "com.mysql.cj.jdbc.Driver"
+  :subprotocol "mysql"
+  :subname (str "//" (env "DB-HOST") ":" (env "DB-PASSWORD")  "/" (env "DB-NAME")  "?useTimeZone=true&serverTimezone=UTC") 
+  :user (env "USER")
+  :password (env "PASSWORD")
+})
+
+(defn execute-query [query]
+  (jdbc/query db query))
+
+(defn get-cursos []
+  (execute-query ["SELECT * FROM cursos"]))
+
+(defn get-curso-by-id [id]
+  (execute-query ["SELECT * FROM cursos WHERE idCurso = ?" id]))
+
+(defroutes app-routes
+  (GET "/cursos" [] (response (get-cursos)))
+  (GET "/curso/:id" [id] (response (get-curso-by-id id)))
+  (route/not-found "Not found"))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Starts the API server"
   [& args]
-  (println "Hello, World!"))
+  (jetty/run-jetty app-routes {:port 3000}))
